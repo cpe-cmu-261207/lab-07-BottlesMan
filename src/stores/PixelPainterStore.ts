b43d57064d46b111bdede898b30c60fed9bf4658
BottlesMan <man2545_@hotmail.com>
2021-09-05 14:13:05 +0700

    PPS

--- src/stores/PixelPainterStore.ts
+++ src/stores/PixelPainterStore.ts
@@ -1,8 +1,11 @@
 import {Store} from 'pullstate'
 
+const color: string[] = ['#000000','#804000','#FE0000','#FE6A00','#FFD800','#00FF01','#FFFFFF','#01FFFF','#0094FE','#0026FF','#B100FE','#FF006E']
+
 type PixelPainterStoreType = {
   //we save painted color as hex code (string) in 2D array
-  canvas: string[][] 
+  canvas: string[][]
+  PickedColor: string
 }
 
 //return an (16 x 16) 2D array filled with "#FFFFFF"
@@ -17,6 +20,34 @@ const createEmptyCanvas = () => {
   return output
 }
 
+export const RandomColor = () =>{
+  const output: string[][] = []
+  for (let i=0; i<16; i++){
+    output[i] = []
+    for (let j=0; j<16; j++){
+      output[i].push(color[Math.floor(Math.random() * 12)])
+    }
+  }
+  return output
+}
+
+export const RandomCell = () =>{
+  PixelPainterStore.update(s => {s.canvas = RandomColor()})
+}
+
+export const ChangeCellColor = (x: number , y: number) =>{
+  PixelPainterStore.update(s =>{s.canvas[y][x] = s.Pickedcolor})
+}
+
+export const ClearColor = () =>{
+  PixelPainterStore.update(s => {s.canvas = createEmptyCanvas()})
+}
+
+export const PickColor = () =>{
+  PixelPainterStore.update(s => {s.Pickedcolor = color})
+}
+
 export const PixelPainterStore = new Store<PixelPainterStoreType>({
-  canvas: createEmptyCanvas()
+  canvas: createEmptyCanvas(),
+  PickedColor: ""
 })
